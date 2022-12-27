188f53847af94e19e88b4975d3b40ea948c3d8c3
jojoc4 <me@jojoc4.ch>
2022-12-27 15:19:12 +0100

    convert remaining docker build to git action

--- Benchmarks/files/docker/run.sh
+++ Benchmarks/files/docker/run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 case "$BENCHMARK" in
 
-0)  docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE registry.jojoc4.ch/tm-hpcc
-    docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE registry.jojoc4.ch/tm-fio
+0)  docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE jojoc4/lsbs-hpcc
+    docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE jojoc4/lsbs-fio
     docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE -e SERVERNAME=$SERVERNAME jojoc4/lsbs-iperf
-    docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE registry.jojoc4.ch/tm-blender
-    docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE registry.jojoc4.ch/tm-dl
+    docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE jojoc4/lsbs-blender
+    docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE jojoc4/lsbs-dl
     docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE jojoc4/lsbs-bdd
-    docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE -e SERVERNAME=$SERVERNAME -p 30500:5000 registry.jojoc4.ch/tm-rest
+    docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE -e SERVERNAME=$SERVERNAME -p 30500:5000 jojoc4/lsbs-rest
     ;;
-1)  docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE registry.jojoc4.ch/tm-hpcc ;;
-2)  docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE registry.jojoc4.ch/tm-fio ;;
+1)  docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE jojoc4/lsbs-hpcc ;;
+2)  docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE jojoc4/lsbs-fio ;;
 3)  docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE -e SERVERNAME=$SERVERNAME jojoc4/lsbs-iperf ;;
-4)  docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE registry.jojoc4.ch/tm-blender ;;
+4)  docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE jojoc4/lsbs-blender ;;
 5)  docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE jojoc4/lsbs-bdd ;;
-6)  docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE registry.jojoc4.ch/tm-dl ;;
-7)  docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE -e SERVERNAME=$SERVERNAME -p 30500:5000 registry.jojoc4.ch/tm-rest ;;
+6)  docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE jojoc4/lsbs-dl ;;
+7)  docker run --rm -e CONTROLPLANEIP=$CONTROLPLANEIP -e SYSTEM=$SYSTEM -e TYPE=$TYPE -e SERVERNAME=$SERVERNAME -p 30500:5000 jojoc4/lsbs-rest ;;
 
 esac
 docker system prune -af
